92c384b06ab93d6f953c6e5de113b68501c664e5
sneha <sneha.kmadhuri@gmail.com>
2025-11-02 20:58:46 +0530

    Fixing minor errors in order-service

--- microservices/user-service/src/server.js
+++ microservices/user-service/src/server.js
@@ -4,9 +4,9 @@ const app = require('./app');
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log('================================================');
+  console.log('------------------------------------------------');
   console.log('User Service running on port ${PORT}');
   console.log('Environment: ${process.env.NODE_ENV}');
   console.log('Health Check: http://localhost:${PORT}/api/users/health');
-  console.log('================================================');
+  console.log('------------------------------------------------');
 });
