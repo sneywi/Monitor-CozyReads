2279958e36a3652e98a5a69382279c8651214cd6
sneha <sneha.kmadhuri@gmail.com>
2025-11-02 21:11:45 +0530

    Error fixed in cart-service

--- microservices/cart-service/src/server.js
+++ microservices/cart-service/src/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
   console.log('------------------------------------------------');
   console.log('Cart Service running on port ${PORT}');
-  console.log(Environment: ${process.env.NODE_ENV}');
+  console.log('Environment: ${process.env.NODE_ENV}');
   console.log('Health Check: http://localhost:${PORT}/api/cart/health');
   console.log('Product Service: ${process.env.PRODUCT_SERVICE_URL}');
   console.log('-------------------------------------------------');
